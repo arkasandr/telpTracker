79056b20c054e91c24525707b2ceb8d1c9a99eb4
arkasandr <arkasandr@gmail.com>
2020-12-26 18:48:43 +0300

    change auth path

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const API_URL = 'http://localhost:8080/api/auth/';
-const API_URL = 'http://195.2.74.216:8881/api/auth/';
+const API_URL = '/api/auth/';
 
 class AuthService {
     login(user) {
