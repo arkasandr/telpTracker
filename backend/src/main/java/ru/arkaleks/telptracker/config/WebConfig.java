edfcd7e95803e1e5c5922a90e42e34b77c476f19
arkasandr <arkasandr@gmail.com>
2020-12-26 15:57:10 +0300

    change cors config

--- backend/src/main/java/ru/arkaleks/telptracker/config/WebConfig.java
+++ backend/src/main/java/ru/arkaleks/telptracker/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("Access-Control-Allow-Origin: *")
                 .allowedHeaders("Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token")
                 .allowedHeaders("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS")
